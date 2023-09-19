3d0956704045318a89e865ad5b0f6d9fe79a13ec
mbashia <mbashiavictor@gmail.com>
2023-09-19 13:15:12 +0300

    js for submitting input using html

--- assets/js/app.js
+++ assets/js/app.js
@@ -26,7 +26,6 @@ import {LiveSocket} from "phoenix_live_view"
 import topbar from "../vendor/topbar"
 
 let csrfToken = document.querySelector("meta[name='csrf-token']").getAttribute("content")
-let liveSocket = new LiveSocket("/live", Socket, {params: {_csrf_token: csrfToken}})
 
 // Show progress bar on live navigation and form submits
 topbar.config({barColors: {0: "#29d"}, shadowColor: "rgba(0, 0, 0, .3)"})
@@ -34,6 +33,32 @@ window.addEventListener("phx:page-loading-start", info => topbar.show())
 window.addEventListener("phx:page-loading-stop", info => topbar.hide())
 
 // connect if there are any LiveViews on the page
+let Hooks = {};
+Hooks.date = {
+    mounted(){
+        var basicDateInput = document.getElementById('basicDate');
+
+    flatpickr(basicDateInput, {
+        enableTime: true,
+        dateFormat: "F, d Y H:i",
+
+    });
+    },
+    updated(){
+        var basicDateInput = document.getElementById('basicDate');
+
+        flatpickr(basicDateInput, {
+            enableTime: false,
+            dateFormat: "F, d Y H:i",
+
+        });
+
+    }
+};
+
+
+let liveSocket = new LiveSocket("/live", Socket, {params: {_csrf_token: csrfToken},  hooks: Hooks,
+})
 liveSocket.connect()
 
 // expose liveSocket on window for web console debug logs and latency simulation:
